8433c5d5b541dcb7648b60a0de8ea3f34fe7d49c
Kirill Vorobiev <kovvok070606@yandex.ru>
2025-11-18 12:05:44 +0300

    feat(auth): add a plug of DashBoardScreen
    Добавил заглушку экрана DashBoard с кнопкой выхода из аккаунта

--- app/src/main/java/com/example/brainracer/ui/screens/DashBoardScreen.kt
+++ app/src/main/java/com/example/brainracer/ui/screens/DashBoardScreen.kt
@@ -1,4 +1,40 @@
 package com.example.brainracer.ui.screens
 
-class DashBoardScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+@Composable
+fun DashBoardScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to Brain Racer", style = MaterialTheme.typography.headlineLarge)
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            Button(onClick = {
+                Firebase.auth.signOut()
+                navController.navigate(BrainRacerScreen.Login.title) {
+                    popUpTo(BrainRacerScreen.DashBoard.title) { inclusive = true }
+                }
+            }) {Text("Logout")}
+        }
+    }
+
 }
