319e92c20367c8e388465f37ec7137a96189e8e4
Екатерина Жук <zukk22708@gmail.com>
2025-12-01 23:30:29 +0300

    feat(repositories): add QuizRepository and QuizRepositoryImpl

--- app/src/main/java/com/example/brainracer/data/repositories/QuizRepository.kt
+++ app/src/main/java/com/example/brainracer/data/repositories/QuizRepository.kt
@@ -1,23 +1,16 @@
 package com.example.brainracer.data.repositories
 
-// TODO
+import com.example.brainracer.domain.entities.ChallengeResult
+import com.example.brainracer.domain.entities.Quiz
+
 interface QuizRepository {
-    // TODO: implement getQuiz func
-
-    // TODO: implement getQuizzesByCategory
-
-    // TODO: implement getQuizzesByUser
-
-    // TODO: implement createQuiz
-
-    // TODO: implement updateQuiz
-
-    // TODO: implement deleteQuiz
-
-    // TODO: implement searchQuizzes
-
-    // TODO: implement recordQuizResult
-
-    // TODO: implement getPopularQuizzes
-
+    suspend fun getQuiz(quizId: String): Result<Quiz>
+    suspend fun getQuizzesByCategory(category: String,limit: Int = 20):Result<List<Quiz>>
+    suspend fun getQuizzesByUser(userId: String): Result<List<Quiz>>
+    suspend fun createQuiz(quiz: Quiz): Result<Unit>
+    suspend fun updateQuiz(quiz: Quiz): Result<Unit>
+    suspend fun deleteQuiz(quizId: String): Result<Unit>
+    suspend fun searchQuizzes(query: String, category: String? = null): Result<List<Quiz>>
+    suspend fun recordQuizResult(quizResult: ChallengeResult): Result<Unit>
+    suspend fun getPopularQuizzes(limit: Int = 10): Result<List<Quiz>>
 }
