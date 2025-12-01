80dc9c36f00d051c25cd6e467c059bc13176f627
Kirill Vorobiev <kovvok070606@yandex.ru>
2025-12-01 16:55:43 +0300

    feat(repositories): add UserRepository and UserRepositoryImpl

---
+++ app/src/main/java/com/example/brainracer/data/repositories/QuizRepository.kt
@@ -0,0 +1,23 @@
+package com.example.brainracer.data.repositories
+
+// TODO
+interface QuizRepository {
+    // TODO: implement getQuiz func
+
+    // TODO: implement getQuizzesByCategory
+
+    // TODO: implement getQuizzesByUser
+
+    // TODO: implement createQuiz
+
+    // TODO: implement updateQuiz
+
+    // TODO: implement deleteQuiz
+
+    // TODO: implement searchQuizzes
+
+    // TODO: implement recordQuizResult
+
+    // TODO: implement getPopularQuizzes
+
+}
