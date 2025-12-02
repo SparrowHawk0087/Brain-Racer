1baff1e4cb3f94f5500baae7f5fc54a24a5eeb10
arzy-pixel <razanovaarina54@gmail.com>
2025-12-02 16:53:02 +0300

    feat: Implement complete Material 3 theme configuration (Colors, Typography, Shapes, Theme)

---
+++ app/src/main/java/com/example/brainracer/ui/theme/Shape.kt
@@ -0,0 +1,32 @@
+package com.example.brainracer.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+
+
+val Shapes = Shapes(
+
+
+    small = RoundedCornerShape(12.dp),
+
+
+    medium = RoundedCornerShape(16.dp),
+
+
+    large = RoundedCornerShape(0.dp)
+)
+
+
+
+
+val ButtonShapeLogin = RoundedCornerShape(20.dp)
+
+val ButtonShapeSignInWithGoogle = RoundedCornerShape(20.dp)
+
+// Кнопка REGISTER
+val ButtonShapeRegister = RoundedCornerShape(24.dp)
+
+// Кнопки RESET PASSWORD и CANCEL
+val ButtonShapeLarge = RoundedCornerShape(30.dp)
--- app/src/main/java/com/example/brainracer/ui/theme/Type.kt
+++ app/src/main/java/com/example/brainracer/ui/theme/Type.kt
@@ -1,34 +1,48 @@
 package com.example.brainracer.ui.theme
 
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import com.example.brainracer.R
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Typography
 
-// Set of Material typography styles to start with
+val Lexend = FontFamily(
+    Font(R.font.lexend_regular),
+    Font(R.font.lexend_bold, FontWeight.Bold)
+)
+
+val BeVietnamPro = FontFamily(
+    Font(R.font.bevietnampro_regular),
+    Font(R.font.bevietnampro_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    displayLarge = TextStyle(
+        fontFamily = Lexend,
+        fontWeight = FontWeight.Bold,
+        fontSize = 28.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = BeVietnamPro,
+        fontWeight = FontWeight.Bold,
+        fontSize = 23.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = Lexend,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+
+
+
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = BeVietnamPro,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
     )
-    */
+
 )
+
+
